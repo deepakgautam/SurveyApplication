a640344d73e336393183526759bc7ac0978b4b68
Deepak Gautam <dgautam@cozeva.com>
2018-05-29 00:08:21 +0530

    bug fixed related to  keys

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
-if (process.env.NODE_ENV === 'productio') {
+if (process.env.NODE_ENV === 'production') {
     module.exports = require('./prod.js');
 } else {
  module.exports = require('./dev.js');
