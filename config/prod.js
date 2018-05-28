139e9faadb64acade4c55ba1cfe9416c1a4ae064
Deepak Gautam <dgautam@cozeva.com>
2018-05-29 00:30:39 +0530

    bug fixed related to  keys

--- config/prod.js
+++ config/prod.js
@@ -1,7 +1,7 @@
 
 module.exports = {
-    googleClientID : process.env.GOOGLE_CLIENT_id,
-    googleClientSecret :PROCESS.ENV.GOOGLE_CLIENT_secretngodb,
-    mongoUri : PROCESS.ENV.MONGO_URI,
-    cookieKey : PROCESS.ENV.COOKIE_KEY
+    googleClientID : process.env.GOOGLE_CLIENT_ID,
+    googleClientSecret :process.env.GOOGLE_CLIENT_SECRET,
+    mongoUri : process.env.MONGO_URI,
+    cookieKey : process.env.COOKIE_KEY
 };
