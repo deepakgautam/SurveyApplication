aff4c637c8a55cfb5527818fe5d230a05be5e256
Deepak Gautam <dgautam@cozeva.com>
2018-05-28 23:51:00 +0530

    dev  and prod  kesys setups

---
+++ config/prod.js
@@ -0,0 +1,7 @@
+
+module.exports = {
+    googleClientID : process.env.GOOGLE_CLIENT_id,
+    googleClientSecret :PROCESS.ENV.GOOGLE_CLIENT_secretngodb,
+    mongoUri : PROCESS.ENV.MONGO_URI,
+    cookieKey : PROCESS.ENV.COOKIE_KEY
+};
